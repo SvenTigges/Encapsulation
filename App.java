ec4deee6a1f1ed8896164a924691f9cfb6dcef13
SvenTigges <sven.tigges@web.de>
2021-06-02 11:45:36 +0200

    02_ Getter + permission

--- App.java
+++ App.java
@@ -4,6 +4,7 @@ public class App {
 	public static void main(String[] args) {
 
 		Test test = new Test(); 
+		ausgabe(test.getTestStr()); 
 		test.setTestStr("Hi");
 		ausgabe(test.getTestStr()); 
 	}
--- Test.java
+++ Test.java
@@ -5,13 +5,19 @@ private String testStr = "Hallo";
 
 // Getter 
 public String getTestStr() {
+  if (this.hasPermisson()) {
     return testStr;
+  } else {
+      return "Sorry, no permission!";
+  }  
 }
 
 // Setter 
 public void setTestStr(String testStr) {
     this.testStr = testStr;
 } 
-
+ private boolean hasPermisson() {
+     return false; 
+ }
     
 }
